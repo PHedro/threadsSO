dcad085e1af0522aa84220d25aa125166090deed
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2013-10-30 21:24:33 -0200

    adiciona chamada a subprocessos simultaneos

--- child_process.py
+++ child_process.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python2.7
 import subprocess
+from subprocess import Popen
 
 #utiliza a lib subprocess do core do python para chamar processos filhos
-#a chamada de um próximo processo só é feita após o encerramento do anterior
-#devido o método call ser uma abstração do Popen(args).wait() que nada mais é
+#a chamada de um proximo processo so e feita apos o encerramento do anterior
+#devido o metodo call ser uma abstracao do Popen(args).wait() que nada mais e
 #que: Popen -> executa processo de acordo com os argumentos passados e wait()
-#espera o término da execução do processo
+#espera o termino da execucao do processo
 subprocess.call('gnome-terminal')
 subprocess.call('gimp')
 subprocess.call('mine')
 subprocess.call('firefox')
+
+#abaixo e mostrado como deve ser feito para abrir subprocessos simultaneos
+#utilizamos diretamente a Popen e omitimos o funcao em cascada wait()
+Popen('gnome-terminal')
+Popen('firefox')
+Popen('mine')
+Popen('gimp')
