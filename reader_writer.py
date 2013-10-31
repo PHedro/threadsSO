d93f5e80929dd0e76d40ab09df7fbee8bb1ed6da
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2013-10-30 22:37:42 -0200

    escritores e leitores com comentarios

--- reader_writer.py
+++ reader_writer.py
@@ -3,6 +3,8 @@ import thread
 import time
 import random
 
+#semaforo file_lock a lib de thread faz o controle por si nao precisa de
+# interferencia do usuario quando nao e possivel adquirir o lock
 file_lock = thread.allocate_lock()
 
 
@@ -16,11 +18,13 @@ def sleep(process):
 def read(reader_number):
     print 'Leitor {0} tentando adquirir semaforo file_lock'.format(reader_number)
     if file_lock.acquire():
+        #abre arquivo para leitura
         file_locked = open('text1', 'r')
         content = file_locked.read()
         file_locked.close()
         print content
         print 'Arquivo lido pelo Leitor {0} liberando file_lock'.format(reader_number)
+        #aguarda um pouco pra liberar o file_lock
         sleep('Leitor {0}'.format(reader_number))
         file_lock.release()
     else:
@@ -46,15 +50,18 @@ def reader_3():
 def write(writer_number):
     print 'Escritor {0} tentando adquirir semaforo file_lock'.format(writer_number)
     if file_lock.acquire():
+        #abre arquivo para leitura
         file_locked = open('text1', 'r')
         content = file_locked.readlines()
         file_locked.close()
+        #abre arquivo para escrita
         file_locked = open('text1', 'w')
         content.append('Arquivo alterado por Escritor {0}'.format(writer_number))
         file_locked.writelines(content)
         file_locked.close()
         print content
         print 'Arquivo lido pelo Escritor {0} liberando file_lock'.format(writer_number)
+        #espera um pouco antes de liberar o file_lock
         sleep('Escritor {0}'.format(writer_number))
         file_lock.release()
     else:
@@ -71,6 +78,7 @@ def writer_2():
     while True:
         write(2)
 
+#inicia as threads
 thread.start_new_thread(reader_1, ())
 thread.start_new_thread(reader_2, ())
 thread.start_new_thread(reader_3, ())
